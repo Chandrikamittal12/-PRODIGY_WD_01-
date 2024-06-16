f936a105528835e228a830f42de3ef345c212764
Chandrikamittal12 <chandrikamittal722@gmail.com>
2024-06-17 01:45:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamb = document.querySelector(".hamb");
+const navList = documen.navList = document.querySelector(".menu-overlay");
+
+hamb.addEventListener("click",function() {
+    navList .classList .toggle("active");
+hamb. classList.toggle ("fixed");
+})
